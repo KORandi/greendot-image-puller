b002b143a4f2c7417845842a98bb248c6db43253
Konstantin Kozokar <konstantin.kozokar@gmail.com>
2021-12-12 22:23:19 +0100

    edit

--- src/Controller/Api.php
+++ src/Controller/Api.php
@@ -3,9 +3,13 @@
 namespace Greendot\ImagePullerClient\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
 class Api extends AbstractController
 {
+    /**
+     * @Route("/qqq", name="qqq")
+     */
     public function index() {
         return $this->json(["status" => 200]);
     }
--- src/ImagePullerClient.php
+++ src/ImagePullerClient.php
@@ -1,11 +1,19 @@
 <?php
 namespace Greendot\ImagePullerClient;
 
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 /**
  * ImagePullerClient
  */
 class ImagePullerClient extends Bundle {
+    public function getContainerExtension(): ?ExtensionInterface
+    {
+        if (null === $this->extension) {
+            $this->extension = new \ImagePullerExtension();
+        }
 
+        return $this->extension ?: null;
+    }
 }
