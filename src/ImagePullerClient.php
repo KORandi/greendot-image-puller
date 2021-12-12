3eb60f689a7c1a2d39bee604e2454febe64eb8a1
Konstantin Kozokar <konstantin.kozokar@gmail.com>
2021-12-12 18:48:43 +0100

    [ADD] Routing configuration

--- src/ImagePullerClient.php
+++ src/ImagePullerClient.php
@@ -1,11 +1,17 @@
 <?php
 namespace Greendot\ImagePullerClient;
 
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 /**
  * ImagePullerClient
  */
 class ImagePullerClient extends Bundle {
-
+    public function load(array $configs, ContainerBuilder $containerBuilder) {
+        $loader = new YamlFileLoader($containerBuilder, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load("routing.yaml");
+    }
 }
