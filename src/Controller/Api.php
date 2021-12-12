d6d0b5161bcb86aef053e101a0e00a8481f270d5
Konstantin Kozokar <konstantin.kozokar@gmail.com>
2021-12-12 23:37:33 +0100

    edit

--- src/Controller/Api.php
+++ src/Controller/Api.php
@@ -5,10 +5,13 @@ namespace Greendot\ImagePullerClient\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/api/gd")
+ */
 class Api extends AbstractController
 {
     /**
-     * @Route("/qqq", name="qqq")
+     * @Route("/", name="index")
      */
     public function index() {
         return $this->json(["status" => 200]);
