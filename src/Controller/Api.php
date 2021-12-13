2175eaf668820b9be0eed8586e24995ec7b35f59
Konstantin Kozokar <konstantin.kozokar@gmail.com>
2021-12-13 01:07:50 +0100

    [ADD] Jwt token

--- src/Controller/Api.php
+++ src/Controller/Api.php
@@ -2,18 +2,39 @@
 
 namespace Greendot\ImagePullerClient\Controller;
 
+use Greendot\ImagePullerClient\Service\JwtService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/api/gd")
+ * @Route("/api/greendot/image-puller-client")
  */
 class Api extends AbstractController
 {
     /**
-     * @Route("/", name="index")
+     * @Route("/", name="index", methods={"GET"})
      */
-    public function index() {
+    public function index(): JsonResponse
+    {
+        return $this->json(["status" => 200]);
+    }
+
+    /**
+     * @Route ("/", name="receive-file", methods={"POST"})
+     */
+    public function receiveFile(
+        Request     $request,
+        JwtService  $jwtService
+    ): JsonResponse
+    {
+        $token = $request->headers->get("jwt-token");
+
+        if (is_null($token) || !$jwtService->isValid($token)) {
+            return $this->json(["status" => 400], 400);
+        }
+
         return $this->json(["status" => 200]);
     }
 }
--- src/DependencyInjection/ImagePullerExtension.php
+++ src/DependencyInjection/ImagePullerExtension.php
@@ -10,10 +10,5 @@ class ImagePullerExtension extends Extension {
     public function load(array $configs, ContainerBuilder $container) {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load("services.yaml");
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
-        foreach ($config as $key => $value) {
-            $container->setParameter('security_headers.' . $key, $value);
-        }
     }
 }
