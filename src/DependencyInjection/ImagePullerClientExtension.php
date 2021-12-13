14c63c8933c1b69359de974bfe832700f6166172
Konstantin Kozokar <konstantin.kozokar@gmail.com>
2021-12-13 11:50:44 +0100

    [ADD] Configuration options

--- src/DependencyInjection/ImagePullerClientExtension.php
+++ src/DependencyInjection/ImagePullerClientExtension.php
@@ -6,7 +6,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-class ImagePullerExtension extends Extension {
+class ImagePullerClientExtension extends Extension {
     public function load(array $configs, ContainerBuilder $container) {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load("services.yaml");
--- src/ImagePullerClientBundle.php
+++ src/ImagePullerClientBundle.php
@@ -2,9 +2,6 @@
 namespace Greendot\ImagePullerClient;
 
 use Greendot\DependencyInjection\Compiler\ContextPass;
-use Greendot\ImagePullerClient\DependencyInjection\ImagePullerExtension;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 /**
