90ec855abd125257e454e69009b5b6880f46c552
Konstantin Kozokar <konstantin.kozokar@gmail.com>
2021-12-13 11:50:01 +0100

    [ADD] Configuration options

---
+++ src/ImagePullerClientBundle.php
@@ -0,0 +1,15 @@
+<?php
+namespace Greendot\ImagePullerClient;
+
+use Greendot\DependencyInjection\Compiler\ContextPass;
+use Greendot\ImagePullerClient\DependencyInjection\ImagePullerExtension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * ImagePullerClient
+ */
+class ImagePullerClientBundle extends Bundle {
+
+}
