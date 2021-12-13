f39d3a29654530aba5b67460b802036f375f8a7d
Konstantin Kozokar <konstantin.kozokar@gmail.com>
2021-12-13 11:29:19 +0100

    [ADD] Configuration options

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Greendot\DependencyInjection;
+namespace Greendot\ImagePullerClient\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
