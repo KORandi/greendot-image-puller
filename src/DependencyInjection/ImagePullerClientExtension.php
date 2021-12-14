02784ff88b982bfb45b74db8c19460b63b4f246b
Konstantin Kozokar <konstantin.kozokar@gmail.com>
2021-12-14 14:49:58 +0100

    [EDIT] Dynamic endpoint from configuration files

--- src/DependencyInjection/ImagePullerClientExtension.php
+++ src/DependencyInjection/ImagePullerClientExtension.php
@@ -10,7 +10,6 @@ class ImagePullerClientExtension extends Extension {
     public function load(array $configs, ContainerBuilder $container) {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load("services.yaml");
-        $loader->load("routes.yaml");
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
         foreach ($config as $key => $value) {
