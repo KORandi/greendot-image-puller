4411a9b65c7c89ad0abf92f99c7d9178ce9061bd
Konstantin Kozokar <konstantin.kozokar@gmail.com>
2021-12-14 17:06:50 +0100

    [ADD] File generator

--- src/ImagePullerClientBundle.php
+++ src/ImagePullerClientBundle.php
@@ -1,11 +1,33 @@
 <?php
 namespace Greendot\ImagePullerClient;
 
+use Greendot\ImagePullerClient\DependencyInjection\Configuration;
+use Symfony\Component\Config\Definition\Processor;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\Yaml\Yaml;
 
 /**
  * ImagePullerClient
  */
 class ImagePullerClientBundle extends Bundle {
 
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+        $processor = new Processor();
+        $configuration = new Configuration();
+        $processedConfigs[] = $processor->processConfiguration($configuration, []);
+        $projectRoot = $this->container->get("kernel.project_dir");
+        $this->saveIntoYAML($processedConfigs, $projectRoot);
+    }
+
+    private function saveIntoYAML(array $array, $projectRoot)
+    {
+        $yaml = Yaml::dump($array);
+        $configPath = "/config/packages/image_puller_client.yaml";
+        if (!file_exists($configPath)) {
+            file_put_contents($this->projectRoot.$configPath, $yaml);
+        }
+    }
 }
